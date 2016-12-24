919c87412f195d426f62d79751e9eeb3fafbff6f
Vi Le <le.vi006@gmail.com>
2016-12-24 15:13:25 -0800

    Modified detail() to use render()

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
 
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the polls index.")
@@ -20,10 +21,16 @@ def vote(request, question_id):
 #     output = ', '.join([q.question_text for q in latest_question_list])
 #     return HttpResponse(output)
 
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
