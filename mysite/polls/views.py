b735c1972590ca9e0ec8a264e4bb92cc55282067
Vi Le <le.vi006@gmail.com>
2016-12-27 14:14:42 -0800

    rewrote details method

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -35,9 +35,13 @@ def vote(request, question_id):
 #     context = {'latest_question_list': latest_question_list}
 #     return render(request, 'polls/index.html', context)
 
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})    
+
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})    
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
